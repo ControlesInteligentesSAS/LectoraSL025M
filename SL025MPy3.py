54236a4a2625549de79e5e15a3743d3c524d89c7
juans912 <juansebastiandiazrincon@gmail.com>
2021-12-17 08:55:21 -0500

    Añadidas Funciones para manejo de lectora

--- SL025MPy3.py
+++ SL025MPy3.py
@@ -212,6 +212,7 @@ def downloadKeyIntoReader(ser:str,sector:bytes,keyType:bytes,key:bytes)->bool:
         return retorno
     
 def loginSectorStoredKey(ser:serial,sector:bytes,keyType:bytes)->bool:
+    retorno=False
     try:
         LOGINCMD=bytearray(b'\xBA\x04\x13'+sector+keyType)
         LOGINCMD.append(CRC(LOGINCMD))
@@ -227,14 +228,330 @@ def loginSectorStoredKey(ser:serial,sector:bytes,keyType:bytes)->bool:
         return retorno
 
 def readDataBlock(ser:serial,block:int)->bytearray:
+    """Lee un bloque de datos
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+        block (int): [Dirección del bloque que se va a leer]
+        
+    Returns:
+        bytearray: [Retorna el bloque de datos si la operacion es exitosa]
+    """ 
     try:
         READCMD=bytearray(b'\xBA\x03\x03')
         READCMD.append(block)
         READCMD.append(CRC(READCMD))
         response=sendCommand(ser,READCMD)
+        print(response)
         if integerToByte(response[0]) == b'\xBD':
             if response[3] != 0:
-                raise Exception(f"Read failed! {STATUS[integerToByte(response[3])]}")
+                raise Exception(f"Read data block failed! {STATUS[integerToByte(response[3])]}")
+            else:
+                return response[4:]
+    except Exception as e:
+        pass
+    finally:
+        pass
+
+def writeDataBlock(ser:serial,block:int,data:list)->bytearray:
+    """Escribe un bloque de datos
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+        block (int): [Dirección del bloque que se va a escribir]
+        data (list): Los datos que se van a escribir
+        
+    Returns:
+        bytearray: [Retorna el bloque de datos escrito si la operacion es exitosa]
+    """ 
+    try:
+        WRITECMD=bytearray(b'\xBA\x13\x04')
+        WRITECMD.append(block)
+        for k in data:
+            WRITECMD.append(k)
+        WRITECMD.append(CRC(WRITECMD))
+        response=sendCommand(ser,WRITECMD)
+        print(response)
+        if integerToByte(response[0]) == b'\xBD':
+            if response[3] != 0:
+                raise Exception(f"Write data block failed! {STATUS[integerToByte(response[3])]}")
+            else:
+                return response[4:]
+    except Exception as e:
+        return (str(e))
+    finally:
+        pass
+
+def readValueBlock(ser:serial,block:int)->bytearray:
+    """Lee el valor de un bloque
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+        block (int): [Dirección del bloque que se va a leer]
+        
+    Returns:
+        bytearray: [Retorna el valor si la operacion es exitosa]
+    """ 
+    try:
+        READCMD=bytearray(b'\xBA\x03\x05')
+        READCMD.append(block)
+        READCMD.append(CRC(READCMD))
+        response=sendCommand(ser,READCMD)
+        print(response)
+        if integerToByte(response[0]) == b'\xBD':
+            if response[3] != 0:
+                raise Exception(f"Read value block failed! {STATUS[integerToByte(response[3])]}")
+            else:
+                return response[4:]
+    except Exception as e:
+        pass
+    finally:
+        pass
+
+def inicializeValueblock(ser:serial,block:int,value:list)->bytearray:
+    """Inicializa el valor de un bloque
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+        block (int): [Dirección del bloque que se va a inicializar]
+        value (list): [El valor a escribir]
+        
+    Returns:
+        bytearray: [Retorna el valor escrito si la operacion es exitosa]
+    """ 
+    try:
+        INITCMD=bytearray(b'\xBA\x07\x06')
+        INITCMD.append(block)
+        for k in value:
+            INITCMD.append(k)
+        INITCMD.append(CRC(INITCMD))
+        response=sendCommand(ser,INITCMD)
+        print(response)
+        if integerToByte(response[0]) == b'\xBD':
+            if response[3] != 0:
+                raise Exception(f"Inicialize value failed! {STATUS[integerToByte(response[3])]}")
+            else:
+                return response[4:]
+    except Exception as e:
+        pass
+    finally:
+        pass
+
+def writeMasterKey(ser:serial,sector:bytes,key:bytes)->bytearray:
+    """Escribe la llave maestra
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+        sector (bytes): [Numero del sector que se va a escribir, 0x00-0x27]
+        key (bytes): [Llave de autenticacion]
+        
+    Returns:
+        bytearray: [Retorna la llave de autenticacion escrita si la operacion es exitosa]
+    """ 
+    try:
+        WRITECMD=bytearray(b'\xBA\x09\x07'+sector+key)
+        WRITECMD.append(CRC(WRITECMD))
+        response=sendCommand(ser,WRITECMD)
+        print(response)
+        if integerToByte(response[0]) == b'\xBD':
+            if response[3] != 0:
+                raise Exception(f"Write master key failed! {STATUS[integerToByte(response[3])]}")
+            else:
+                return response[4:]
+    except Exception as e:
+        return (str(e))
+    finally:
+        pass
+
+def incrementValue(ser:serial,block:int,value:list)->bytearray:
+    """Incrementa el valor
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+        block (int): [Dirección del bloque que se va a incrementar]
+        value (list): [El valor a ser incrementado]
+        
+    Returns:
+        bytearray: [Retorna el valor despues del incremento si la operacion es exitosa]
+    """ 
+    try:
+        INCREMCMD=bytearray(b'\xBA\x07\x08')
+        INCREMCMD.append(block)
+        for k in value:
+            INCREMCMD.append(k)
+        INCREMCMD.append(CRC(INCREMCMD))
+        response=sendCommand(ser,INCREMCMD)
+        print(response)
+        if integerToByte(response[0]) == b'\xBD':
+            if response[3] != 0:
+                raise Exception(f"Increment value failed! {STATUS[integerToByte(response[3])]}")
+            else:
+                return response[4:]
+    except Exception as e:
+        pass
+    finally:
+        pass
+
+def decrementValue(ser:serial,block:int,value:list)->bytearray:
+    """Incrementa el valor
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+        block (int): [Dirección del bloque que se va a decrementar]
+        value (list): [El valor a ser decrementado]
+        
+    Returns:
+        bytearray: [Retorna el valor despues del decremento si la operacion es exitosa]
+    """ 
+    try:
+        DECREMCMD=bytearray(b'\xBA\x07\x09')
+        DECREMCMD.append(block)
+        for k in value:
+            DECREMCMD.append(k)
+        DECREMCMD.append(CRC(DECREMCMD))
+        response=sendCommand(ser,DECREMCMD)
+        print(response)
+        if integerToByte(response[0]) == b'\xBD':
+            if response[3] != 0:
+                raise Exception(f"Decrement value failed! {STATUS[integerToByte(response[3])]}")
+            else:
+                return response[4:]
+    except Exception as e:
+        pass
+    finally:
+        pass
+
+def copyValue(ser:serial,source:int,destination:int)->bytearray: #PENDIENTE
+    """Copia el valor
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+        source (int): [Fuente desde donde el bloque se va a copiar]
+        destination (int): [Destino a donde se va a copiar]
+        La fuente y el destino deben estar en el mismo sector
+        
+    Returns:
+        bytearray: [Retorna el valor despues de la copia si la operacion es exitosa]
+    """ 
+    try:
+        COPYCMD=bytearray(b'\xBA\x04\x0A')
+        COPYCMD.append(source)
+        COPYCMD.append(destination)
+        COPYCMD.append(CRC(COPYCMD))
+        print(COPYCMD)
+        response=sendCommand(ser,COPYCMD)
+        print(response)
+        if integerToByte(response[0]) == b'\xBD':
+            if response[3] != 0:
+                raise Exception(f"Copy value failed! {STATUS[integerToByte(response[3])]}")
+            else:
+                return response[4:]
+    except Exception as e:
+        pass
+    finally:
+        pass
+
+def readDataPage(ser:serial,page:int)->bytearray:
+    """Lee una pagina de datos (UltraLight & NTAG203)
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+        page (int): [Numero de página a leer, 0x00-0x0F]
+        
+    Returns:
+        bytearray: [Retorna el bloque de datos si la operacion es exitosa]
+    """ 
+    try:
+        READCMD=bytearray(b'\xBA\x03\x10')
+        READCMD.append(page)
+        READCMD.append(CRC(READCMD))
+        print(READCMD)
+        response=sendCommand(ser,READCMD)
+        print(response)
+        if integerToByte(response[0]) == b'\xBD':
+            if response[3] != 0:
+                raise Exception(f"Read data page failed! {STATUS[integerToByte(response[3])]}")
+            else:
+                return response[4:]
+    except Exception as e:
+        pass
+    finally:
+        pass
+
+def writeDataPage(ser:serial,page:int,data:list)->bytearray: #PENDIENTE
+    """Escribe una pagina de datos (UltraLight & NTAG203)
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+        page (int): [Numero de página a escribir, 0x00-0x0F]
+        data (list): [Los datos a escribir]
+        
+    Returns:
+        bytearray: [Retorna los datos de la pagina escrita si la operacion es exitosa]
+    """
+    try:
+        WRITECMD=bytearray(b'\xBA\x07\x11')
+        WRITECMD.append(page)
+        for k in data:
+            WRITECMD.append(k)
+        WRITECMD.append(CRC(WRITECMD))
+        print(WRITECMD)
+        response=sendCommand(ser,WRITECMD)
+        print(response)
+        if integerToByte(response[0]) == b'\xBD':
+            if response[3] != 0:
+                raise Exception(f"Write data page failed! {STATUS[integerToByte(response[3])]}")
+            else:
+                return response[4:]
+    except Exception as e:
+        pass
+    finally:
+        pass
+
+def manageRedLed(ser:serial,code:bytes)->bool:
+    """Manejo del led rojo
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+        code (int): [0 para led apagado, otro para led encendido]
+        
+    Returns:
+        bool: [Retorna True si la sesión fue exitosa, False si no]
+    """
+    retorno= False
+    try:
+        MANAGECMD=bytearray(b'\xBA\x03\x40'+code)
+        MANAGECMD.append(CRC(MANAGECMD))
+        response=sendCommand(ser,MANAGECMD)
+        print(response)
+        if integerToByte(response[0]) == b'\xBD':
+            if response[3] != 0:
+                raise Exception(f"Manage red led failed! {STATUS[integerToByte(response[3])]}")
+            else:
+                retorno = True
+                return retorno
+    except Exception as e:
+        pass
+    finally:
+        return retorno
+
+def getFimwareVersion(ser:serial)->bytearray:
+    """Se obtiene la version del Fimware
+
+    Args:
+        ser (str): Puerto donde está conectada la lectora
+                
+    Returns:
+        bytearray: [Retorna la version del fimware si la operacion es exitosa]
+    """
+    try:
+        GETCMD=bytearray(b'\xBA\x02\xF0')
+        GETCMD.append(CRC(GETCMD))
+        response=sendCommand(ser,GETCMD)
+        print(response)
+        if integerToByte(response[0]) == b'\xBD':
+            if response[3] != 0:
+                raise Exception(f"Get fimware version failed! {STATUS[integerToByte(response[3])]}")
             else:
                 return response[4:]
     except Exception as e:
@@ -243,26 +560,80 @@ def readDataBlock(ser:serial,block:int)->bytearray:
         pass
 
 serialObject=getSerialObject()
-#loginSector(serialObject,b'\x00',b'\xAA',hexToBytes("C09B3755B261"))
+loginSector(serialObject,b'\x01',b'\xAA',hexToBytes("852D76D7634E"))
 #selectCard(findPort())
 #(b'\r\xaa\x115\xa0\xcf')
-downloadKeyIntoReader(serialObject,b'\x00',b'\xAA',hexToBytes("055EAC1B6339"))
-loginSectorStoredKey(serialObject,b'\x00',b'\xAA')
-respuesta=(readDataBlock(serialObject,1))
-respuesta2=[]
-for k in respuesta:
-    respuesta2.append(k)
-def byteToHex(byte:bytes)->str:
-    return ''.join(format(b, '02x') for b in byte)
-print(byteToHex(respuesta))
-print(respuesta2)
-#arreglo=[]
-#for k in respuesta:
-    #arreglo.append(k)
-#print(arreglo)
-#print(integerToByte(9))
-#print(byteToInteger(b'\x12'))
-#print(integerToByte(4))
+downloadKeyIntoReader(serialObject,b'\x01',b'\xAA',hexToBytes("852D76D7634E"))
+loginSectorStoredKey(serialObject,b'\x01',b'\xAA')
+
+# respuesta=(readDataBlock(serialObject,1))
+# respuesta2=[]
+# for k in respuesta:
+#     respuesta2.append(k)
+# def byteToHex(byte:bytes)->str:
+#     return ''.join(format(b, '02x') for b in byte)
+# print("LECTURAA")
+# print(byteToHex(respuesta))
+# print(respuesta)
+# print(respuesta2)
+
+# respuesta2=[146, 9, 250, 113, 19, 7, 0, 1, 0, 0, 0, 0, 2, 1, 0, 0, 171]
+# respuesta2.pop()
+# escritura=(writeDataBlock(serialObject,1,respuesta2))
+# print("ESCRITURA")
+# print(escritura)
+
+# readValue= readValueBlock(serialObject,2)
+# print(readValue)
+
+#value=[32,54,67,5]
+# inicializeValue = inicializeValueblock(serialObject,1,value)
+# print(inicializeValue)
+
+# writeKey=writeMasterKey(serialObject,b'\x00',hexToBytes("C09B3755B261"))
+# print(writeKey)
+
+# value=[32,54,67,5]
+# incrementV = incrementValue(serialObject,2,value)
+# print(incrementV)
+
+# value=[32,54,67,5]
+# decrementV = decrementValue(serialObject,1,value)
+# print(decrementV)
+
+# copyV = copyValue(serialObject,0,0)
+# print("VALOR")
+# print(copyV)
+
+# readDP = readDataPage(serialObject,1)
+# print(readDP)
+# res=[]
+# for k in readDP:
+#     res.append(k)
+
+# res.pop()
+# print(res)
+
+# data=[4,3,2,5]
+# writeDP = writeDataPage(serialObject,1,data)
+# print(writeDP)
+
+# manageLed = manageRedLed(serialObject,b'\x01')
+# print(manageLed)
+
+# getFimware= getFimwareVersion(serialObject)
+# print(getFimware)
+
+
+
+
+# arreglo=[]
+# for k in respuesta:
+#     arreglo.append(k)
+# print(arreglo)
+# print(integerToByte(9))
+# print(byteToInteger(b'\x12'))
+# print(integerToByte(4))
 
 """ lista=[186,3,3,3]
 bytelista=bytearray()
